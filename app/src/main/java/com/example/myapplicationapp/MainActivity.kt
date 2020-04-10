410d161de4d224b2545e7d50c0a965df4a27c9cf
user <seoleo96@gmail.com>
2020-04-10 15:46:38 +0600

    init commit

--- app/src/main/java/com/example/myapplicationapp/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myapplicationapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textview.text = "Hello Guys !"
     }
+
+
 }
